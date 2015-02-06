c6f5ee36736efdce43789b278334b7046e9edefc
Ross Pokorny <ross.pokorny@nextcentury.com>
2015-02-06 09:20:50 -0700

    feat(Profile Window): Fix api for users who own a single listing

--- app/js/api/Profile.js
+++ app/js/api/Profile.js
@@ -7,7 +7,7 @@ var { API_URL } = require('../OzoneConfig');
 var ProfileApi = {
     getOwnedListings: function (profileId) {
     	return $.getJSON(`${API_URL}/api/profile/${encodeURIComponent(profileId)}/listing`)
-            .then(resp => (resp._embedded ? resp._embedded.item : null) || []);
+            .then(resp => (resp._embedded ? [].concat(resp._embedded.item) : null) || []);
     },
 
     getProfile: function (profileId) {
