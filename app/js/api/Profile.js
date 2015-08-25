4b60ff925303257ec6a4be3521531749e2fdd997
Blaine Nelson <blaine.m.nelson@gmail.com>
2015-08-25 11:30:51 -0400

    feat(quickview): Convert backend data with humps OZBE#21, 35
        OZBE21 = ozone-development/ozp-backend#21
        OZBE35 = ozone-development/ozp-backend#35
    
    Also update roles checkers.

--- app/js/api/Profile.js
+++ app/js/api/Profile.js
@@ -2,16 +2,19 @@
 
 var $ = require('jquery');
 var Response = require('./responses/Response');
+var humps = require('humps');
 
 var { API_URL } = require('../OzoneConfig');
 
 var ProfileApi = {
     getOwnedListings: function () {
-        return $.getJSON(`${API_URL}/api/self/listing/`);
+        return $.getJSON(`${API_URL}/api/self/listing/`).then(
+            (resp) => humps.camelizeKeys(resp));
     },
 
     getProfile: function () {
-        return $.getJSON(`${API_URL}/api/self/profile/`);
+        return $.getJSON(`${API_URL}/api/self/profile/`).then(
+            (resp) => humps.camelizeKeys(resp));
     },
 
     updateProfile: function (profileData) {
